412801474e930a5614cb52c63a3460328626fdcb
Victor Matheus <victormatheus507@gmail.com>
2024-04-05 16:58:23 -0300

    pagina de login

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,13 +1,23 @@
-import { Outlet } from "react-router-dom"
+import { Outlet } from 'react-router-dom';
+import { Pizza } from 'lucide-react';
 
 export function AuthLayout() {
   return (
-    <div>
-      <h1>Autentificação</h1>
+    <div className="min-h-screen grid grid-cols-2">
+      <div className="h-full border-r border-foreground/5 bg-muted p-10 text-muted-foreground flex flex-col justify-between">
+        <div className="flex items-center gap-3 text-lg text-foreground">
+          <Pizza className='h-5 2-5' />
+          <span className='font-semibold'>Pizza Shop</span>
+        </div>
 
-      <div>
-        <Outlet/>
+        <footer className="text-sm">
+          Painel do parceiro &:copy; Pizza Shop - {new Date().getFullYear()}
+        </footer>
+      </div>
+
+      <div className='flex flex-col items-center justify-center'>
+        <Outlet />
       </div>
     </div>
-  )
+  );
 }
