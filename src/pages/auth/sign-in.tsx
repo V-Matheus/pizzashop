df16173ea9cf00366786e4f14afe9d982a7645b9
Victor Matheus <victormatheus507@gmail.com>
2024-04-05 17:37:49 -0300

    Cadastro de restaurante

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -3,33 +3,36 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@radix-ui/react-label';
 import { Helmet } from 'react-helmet-async';
 
-import {useForm} from 'react-hook-form'
-import {z} from 'zod'
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
 import { toast } from 'sonner';
+import { Link } from 'react-router-dom';
 
 const signInForm = z.object({
   email: z.string().email(),
-})
+});
 
-type SignInForm = z.infer<typeof signInForm>
+type SignInForm = z.infer<typeof signInForm>;
 
-export function SigIn() {
-
-  const {register, handleSubmit, formState: {isSubmitting}} = useForm<SignInForm>()
+export function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>();
 
   async function handleSignIn(data: SignInForm) {
     try {
-      await new Promise(resovle => setTimeout(resovle, 2000))
-  
+      await new Promise((resovle) => setTimeout(resovle, 2000));
+
       toast.success('Enviamos um link de autentificação para o teu E-mail.', {
         action: {
           label: 'Reenviar',
-          onClick: () => handleSignIn(data)
-        }
-      })
-    } catch(err) {
-      toast.error('Credenciais inválidas.')
-
+          onClick: () => handleSignIn(data),
+        },
+      });
+    } catch (err) {
+      toast.error('Credenciais inválidas.');
     }
   }
 
@@ -37,24 +40,29 @@ export function SigIn() {
     <div>
       <Helmet title="Login" />
       <div className="p-8">
+        <Button variant="ghost" asChild className="absolute right-8 top-8">
+          <Link to="/sign-up">Novo estabelecimento</Link>
+        </Button>
+
         <div className="w-[350px] flex flex-col justify-center gap-6">
-          <div className="flex flex-col gap-2 text-center"></div>
-          <h1 className="text-2xl font-semibold tracking-tighter">
-            Acessar painel
-          </h1>
-          <p className="text-sm text-muted-foreground">
-            Acompanhe suas vendas pelo painel do parceiro!
-          </p>
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tighter">
+              Acessar painel
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Acompanhe suas vendas pelo painel do parceiro!
+            </p>
+          </div>
 
-          <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4" >
-
-            <div className='space-y-2'>
-              <Label htmlFor='email'>Seu email</Label>
-              <Input id='email' type='email' {...register('email')}/>
+          <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu email</Label>
+              <Input id="email" type="email" {...register('email')} />
             </div>
 
-            <Button disabled={isSubmitting} className='w-full' type='submit'>Acessar painel</Button>
-
+            <Button disabled={isSubmitting} className="w-full" type="submit">
+              Acessar painel
+            </Button>
           </form>
         </div>
       </div>
