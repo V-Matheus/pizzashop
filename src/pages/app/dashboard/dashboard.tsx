07a91c3b6f7209d8e5634a901c8ae67c43a195b9
Victor Matheus <victormatheus507@gmail.com>
2024-04-05 22:10:07 -0300

    grafico de receita no periodo

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -3,6 +3,7 @@ import { MonthRevenueCard } from './month-revenue-card';
 import { MonthOrdersAmountCard } from './month-orders-amount-card';
 import { DaysOrdersAmountCard } from './days-orders-amount-card';
 import { MonthCanceledOrdersCard } from './month-canceled-orders-amount-card';
+import { RevenueChart } from './revenue-chart';
 
 export function Dashboard() {
   return (
@@ -16,6 +17,10 @@ export function Dashboard() {
           <MonthOrdersAmountCard/>
           <DaysOrdersAmountCard/>
           <MonthCanceledOrdersCard/>
+        </div>
+
+        <div className='grid grid-col-9 gap-4 gap-4'>
+          <RevenueChart/>
         </div>
       </div>
     </>
