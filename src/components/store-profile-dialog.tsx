c3cde2d4f16f39552a5d87b1348307097684c52a
Victor Matheus <victormatheus507@gmail.com>
2024-04-09 11:23:39 -0300

    Modal de atualização do perfil

---
+++ src/components/store-profile-dialog.tsx
@@ -0,0 +1,70 @@
+import { DialogDescription, DialogTitle } from '@radix-ui/react-dialog';
+import { DialogContent, DialogFooter, DialogHeader } from './ui/dialog';
+import { Button } from './ui/button';
+import { Label } from './ui/label';
+import { Input } from './ui/input';
+import { Textarea } from './ui/textarea';
+import { useQuery } from '@tanstack/react-query';
+import { getManagedRestaurant } from '@/api/get-managed-restaurant';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const storeProfileScheme = z.object({
+  name: z.string().min(1),
+  description: z.string(),
+});
+
+type StoreProfileScheme = z.infer<typeof storeProfileScheme>;
+
+export function StoreProfileDialog() {
+  const { data: managedRestaurant } = useQuery({
+    queryKey: ['managed-restaurant'],
+    queryFn: getManagedRestaurant,
+  });
+
+  const { register, handleSubmit } = useForm<StoreProfileScheme>({
+    resolver: zodResolver(storeProfileScheme),
+    values: {
+      name: managedRestaurant?.name ?? '',
+      description: managedRestaurant?.description ?? '',
+    },
+  });
+
+  return (
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Perfil da loja</DialogTitle>
+        <DialogDescription>
+          Atualize as informações do seu estabelecimento visíveis ao seu cliente
+        </DialogDescription>
+      </DialogHeader>
+
+      <form>
+        <div className="space-y-4 gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label className="text-right" htmlFor="name">
+              Nome
+            </Label>
+            <Input className="col-span-3" id="name" {...register('name')} />
+          </div>
+
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label className="text-right" htmlFor="description">
+              Descrição
+            </Label>
+            <Textarea className="col-span-3" id="description" {...register('description')}/>
+          </div>
+        </div>
+        <DialogFooter>
+          <Button variant="ghost" type="button">
+            Cancelar
+          </Button>
+          <Button type="submit" variant="success">
+            Salvar
+          </Button>
+        </DialogFooter>
+      </form>
+    </DialogContent>
+  );
+}
